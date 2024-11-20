4d44abc1091614d0d6eb7be4d8bb8a685999b09e
Amin Ehsan <Mr.AminEhsan@gmail.com>
2024-11-20 13:03:27 +0330

    Update logstash_logger.py

--- logstash_logger.py
+++ logstash_logger.py
@@ -6,36 +6,36 @@ class LogstashLogger:
     """A Python class for interacting with Logstash, a logger."""
 
     def __init__(self, host: str, port: int, level: int, project: str, name: str) -> None:
-        self.__setup(name=name, level=level, host=host, port=port)
-        self.__extra = {'project_name': project}
+        self._setup(name=name, level=level, host=host, port=port)
+        self._extra = {'project_name': project}
 
-    def __setup(self, name: str, level: int | str, host: str, port: int):
+    def _setup(self, name: str, level: int | str, host: str, port: int):
         """Set up the logger."""
-        self.__logger = logging.getLogger(name)
-        self.__logger.setLevel(level)
-        self.__logger.addHandler(logstash.TCPLogstashHandler(host=host, port=port, version=1))
+        self._logger = logging.getLogger(name)
+        self._logger.setLevel(level)
+        self._logger.addHandler(logstash.TCPLogstashHandler(host=host, port=port, version=1))
 
     def debug(self, msg: str = '', extra: dict = None) -> None:
         extra = extra or dict()
-        extra.update(self.__extra)
-        self.__logger.debug(msg=msg, exc_info=True, stack_info=True, extra=extra)
+        extra.update(self._extra)
+        self._logger.debug(msg=msg, exc_info=True, stack_info=True, extra=extra)
 
     def info(self, msg: str = '', extra: dict = None) -> None:
         extra = extra or dict()
-        extra.update(self.__extra)
-        self.__logger.info(msg=msg, exc_info=True, stack_info=True, extra=extra)
+        extra.update(self._extra)
+        self._logger.info(msg=msg, exc_info=True, stack_info=True, extra=extra)
 
     def warning(self, msg: str = '', extra: dict = None) -> None:
         extra = extra or dict()
-        extra.update(self.__extra)
-        self.__logger.warning(msg=msg, exc_info=True, stack_info=True, extra=extra)
+        extra.update(self._extra)
+        self._logger.warning(msg=msg, exc_info=True, stack_info=True, extra=extra)
 
     def error(self, msg: str = '', extra: dict = None) -> None:
         extra = extra or dict()
-        extra.update(self.__extra)
-        self.__logger.error(msg=msg, exc_info=True, stack_info=True, extra=extra)
+        extra.update(self._extra)
+        self._logger.error(msg=msg, exc_info=True, stack_info=True, extra=extra)
 
     def critical(self, msg: str = '', extra: dict = None) -> None:
         extra = extra or dict()
-        extra.update(self.__extra)
-        self.__logger.critical(msg=msg, exc_info=True, stack_info=True, extra=extra)
+        extra.update(self._extra)
+        self._logger.critical(msg=msg, exc_info=True, stack_info=True, extra=extra)
